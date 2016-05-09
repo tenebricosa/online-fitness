3305e25f3065f3429b32f502f3c30be9a2e43949
Анастасия Сахарова <elliot.wrt@gmail.com>
2016-05-09 12:55:02 +0500

    some adds

--- src/containers/Goals/Goals.js
+++ src/containers/Goals/Goals.js
@@ -7,9 +7,9 @@ import style from './Goals.scss';
 export default class Goals extends Component {
     render() {
         return (
-            <div className={style.wrapper}>
+            <section className={style.wrapper}>
                 <BlockTitle>Мы вместе поставим цель</BlockTitle>
-            </div>
+            </section>
         );
     }
 }
