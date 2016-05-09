b4300a24c17004062242b9f273a108c74fb1b81d
Анастасия Сахарова <elliot.wrt@gmail.com>
2016-05-09 12:45:10 +0500

    add normalize

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
-import Intro from '../components/Intro/Intro'
+import Intro from 'components/Intro/Intro'
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                <div>Привет из App</div>
                 <Intro />
             </div>
         )
