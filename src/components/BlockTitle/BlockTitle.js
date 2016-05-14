b76eac70c78c67e7306e3389680ca521097900bf
Анастасия Сахарова <elliot.wrt@gmail.com>
2016-05-14 19:03:08 +0500

    Расширила компонент BlockTitle

--- src/components/BlockTitle/BlockTitle.js
+++ src/components/BlockTitle/BlockTitle.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
 
 import style from './BlockTitle.scss';
 
 export default class BlockTitle extends Component {
     render() {
+        const { isLight } = this.props;
+
         return(
-          <h2 className={style.title}>{this.props.children}</h2>
+          <h2 className={classnames(style.title, isLight ? style.light : '')}>{this.props.children}</h2>
         );
     }
+}
+
+BlockTitle.propTypes = {
+    isLight: PropTypes.bool
+}
+
+
+BlockTitle.defaultProps = {
+    isLight: false
 }
